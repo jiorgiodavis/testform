cf57bfaefb5b5abe5f261ba24558ac6313423602
Jiorgio Alvarez <jiorgiodavis@gmail.com>
2018-04-13 20:20:28 +0300

    form updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @stack('styles')
 </head>
 <body>
     <div id="app">
--- resources/views/sample/create.blade.php
+++ resources/views/sample/create.blade.php
@@ -3,37 +3,60 @@
 @section("content")
 <div class='container'>
     <form class='form' id="frmUser" method="post" enctype="multipart/form-data">
-        <div class="form-group">
-        <label for="">First Name</label>
-        <input type="text" class="form-control" name="first_name" id="first_name" placeholder="First Name" required>
-            <small class="form-text text-muted"></small>
+        
+        <div class='col-lg-6 col-md-6 col-sm-6'>
+            <div class="form-group">
+            <label for="">First Name</label>
+                <input type="text" class="form-control" name="first_name" id="first_name" placeholder="First Name" required>
+                <small class="form-text text-muted"></small>
+            </div>
         </div>
-        <div class="form-group">
-        <label for="">Last Name</label>
-        <input type="text" class="form-control" name="last_name" id="last_name" placeholder="Last Name.." required>
-            <small class="form-text text-muted"></small>
+        <div class='col-lg-6 col-md-6 col-sm-6'>
+            <div class="form-group">
+            <label for="">Last Name</label>
+            <input type="text" class="form-control" name="last_name" id="last_name" placeholder="Last Name.." required>
+                <small class="form-text text-muted"></small>
+            </div>
         </div>
-        <div class="form-group">
-        <label for="">Email</label>
-        <input type="email" class="form-control" name="email" id="email" placeholder="Email" required>
-            <small class="form-text text-muted"></small>
+
+        <div class='col-lg-6 col-md-6 col-sm-6'>
+            <div class="form-group">
+            <label for="">Email</label>
+            <input type="email" class="form-control" name="email" id="email" placeholder="Email" required>
+                <small class="form-text text-muted"></small>
+            </div>
         </div>
-        <div class="form-group">
-        <label for="">Password</label>
-        <input type="password" class="form-control" name="password" id="password" placeholder="Password.." required>
-            <small class="form-text text-muted"></small>
+        <div class='col-lg-6 col-md-6 col-sm-6' >
+            <div class="form-group">
+            <label for="">Password</label>
+            <input type="password" class="form-control" name="password" id="password" placeholder="Password.." required>
+                <small class="form-text text-muted"></small>
+            </div>
         </div>
-        <div class="form-group">
-        <label for="">Username</label>
-        <input type="text" class="form-control" name="username" id="username" placeholder="Username.." required>
-            <small class="form-text text-muted"></small>
+
+        <div class='col-lg-6 col-md-6 col-sm-6'>
+            <div class="form-group">
+            <label for="">Username</label>
+            <input type="text" class="form-control" name="username" id="username" placeholder="Username.." required>
+                <small class="form-text text-muted"></small>
+            </div>
         </div>
-        <div class="form-group">
-        <label for="">Date of Birth</label>
-        <input type="text" class="form-control" name="dob" id="dob" placeholder="Date of Birth..." required>
-            <small class="form-text text-muted"></small>
+        <div class='col-lg-6 col-md-6 col-sm-6' >
+            <div class="form-group">
+            <label for="">Date of Birth</label>
+            <input type="text" class="form-control" name="dob" id="dob" placeholder="Date of Birth..." required>
+                <small class="form-text text-muted"></small>
+            </div>
         </div>
-        <button type="button" class='btn btn-primary btn-block btnSave'>Save</button>
+
+
+        
+        
+        
+        
+        
+        
+        <button type="button" class='btn btn-primary btn-center btnSave'>Save</button>
     </form>
 </div>
 @endsection
@@ -42,27 +65,39 @@
     <script>
         $(function() {
             $(".btnSave").click(function() { $("#frmUser").submit(); });
-            $(".form-control").focus(function() {
+            $(".form-control").keypress(function() {
                 $(this).next().html("");
+                $(this).prev(".arrow-indicator, .arrow-indicator-right").remove();
             });
             $("#frmUser").submit(function() 
             {
                 var context = $(this);
                 var hasEmptyField = false;
+                $(".arrow-indicator, .arrow-indicator-right").remove();
+                var counter = 0;
                 $("[required]",context).each(function()
                 {
-                    if($.trim($(this).val()) == "" && hasEmptyField == false)
+                    if($.trim($(this).val()) == "")
                     {
+                        if(counter%2==1)
+                        {
+                            $("<i class='fa fa-arrow-left fa-2x arrow-indicator-right'></i>").insertBefore($(this));
+                        } else {
+                            $("<i class='fa fa-arrow-right fa-2x arrow-indicator'></i>").insertBefore($(this));
+                        }
+                        
+                        $(".arrow-indicator, .arrow-indicator-right").animate({"opacity":"1"},500);
                         $(this).next().html("<i class='fa fa-angle-right'></i>&nbsp;This field is required.");
                         hasEmptyField = true;                        
                     }
+                    counter++;
                 });
                 if(hasEmptyField)
                 {
                     return false;
                 }
             });
-        })(jQuery);
+        });
     </script>
 @endpush
 
